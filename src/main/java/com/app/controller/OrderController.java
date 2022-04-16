85d900b7db722bc45bf5d9aa824c7705c5af0e03
kumardeepaksharma <ms893694@gmail.com>
2022-04-16 10:03:08 +0000

    first commit

---
+++ src/main/java/com/app/controller/OrderController.java
@@ -0,0 +1,22 @@
+package com.app.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/order")
+public class OrderController {
+	
+	
+	@Value("${server.port}")
+	private String port;
+	
+	
+	@GetMapping("/status")
+	public String getOrderStatus() {
+	return "FINISHED:\"Hello from Order Provider\":"+port;
+	}
+
+}
